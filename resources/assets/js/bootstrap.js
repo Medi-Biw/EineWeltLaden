f49312753f86b7354471fe30164fd7dbfc83f4f5
Even0900 <github@gaspla.net>
2017-12-29 02:25:39 +0100

    Laravel Makeover

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,9 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
+	window.Popper = require('popper.js').default;
+	
+    require('bootstrap');
 } catch (e) {}
 
 /**
